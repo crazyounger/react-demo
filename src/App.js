aa46e0a0c1c4e627e9aaafd180e167d7050556d0
赵鹏飞 <pengfei.zhao@fuwo.com>
2018-10-11 15:30:47 +0800

    multi components

--- src/App.js
+++ src/App.js
@@ -1,69 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React,{Component} from "react";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
+class App extends Component {
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
+  render(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <Header />
+          </div>
+        </div>
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hello !!!</h1>
+          </div>
+        </div>
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
-);
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <Home />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React ,{ Component } from 'react';
+
+class Home extends Component{
+
+  render(){
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hi , This is Home Page</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
